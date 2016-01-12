95a898a3f776f5adb5c781489137d8c948e20473
Froehlich <Steven.Froehlich@br.de>
2016-01-12 12:30:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+The "walkthru" of the game:
+
+
+
+1) With the actor you have to reach the water.	
+
+2) While moving across the road you have to try not to get in touch with the bugs.	
+
+3) Move the actor with the following keys:
+	
+	- "up" for moving up
+	
+	- "down" for moving down
+	
+	- "left" for moving left
+	
+	- "right" for moving right
+
+4) Reaching the water, you have won the game.
---
+++ js/app.js
@@ -0,0 +1,109 @@
+// Enemies our player must avoid
+var Enemy = function(x,y) {
+// Variables applied to each of our instances go here,
+// we've provided one for you to get started
+
+// The image/sprite for our enemies, this uses
+// a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+// You should multiply any movement by the dt parameter
+// which will ensure the game runs at the same speed for
+// all computers.
+    this.x += 300*dt;
+    if (this.x > 505){
+        this.x = -101;
+    }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Actor = function(x, y){
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+    }
+
+Actor.prototype.update = function() {
+//Look's up if the player get's in touch with any enemy
+    for (i in allEnemies){
+        enemy = allEnemies[i];
+        if (this.x < (enemy.x+60) && this.x > (enemy.x - 70) && this.y < (enemy.y+20) && this.y > (enemy.y - 20))
+            {
+//If it's the case, the player get's position
+            this.x = 200;
+            this.y = 300;
+            }
+        }
+//If the player has passed without collision, he will be returned back to his position, and a new set of enemies will be produced
+    if (this.y < 35){
+            this.x = 200;
+            this.y = 300;
+            generateEnemies();
+        }
+}
+
+//The player to be displayed
+Actor.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+//The player to be moved
+Actor.prototype.handleInput = function(key){
+    if(key === 'left' && this.x > 35){
+        this.x -=100;
+    }
+    if(key === 'up' && this.y > 35){
+        this.y -= 82.5;
+    }
+    if(key === 'right' && this.x < 390){
+        this.x += 100;
+    }
+    if(key === 'down' && this.y < 355){
+        this.y +=82.5;
+    }
+}
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called actor
+var allEnemies;
+var actor;
+
+//Set the sequence of the enemies
+var generateEnemies = function(){
+    allEnemies = [new Enemy(200*Math.random()*2, 50), new Enemy(100*Math.random()*2, 200), new Enemy(300*Math.random()*2, 50),
+        new Enemy(400*Math.random()*2, 150)];
+}
+
+var init = function() {
+    generateEnemies();
+    actor = new Actor(200, 300);
+}
+
+init();
+
+// This listens for key presses and sends the keys to your
+// actor.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    actor.handleInput(allowedKeys[e.keyCode]);
+});
